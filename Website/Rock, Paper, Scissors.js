7614bf83f153a16eb99e4d0fc52444b47f3fa079
Shoom01 <106492636+Shoom01@users.noreply.github.com>
2024-07-25 14:15:53 +0100

    Update Rock, Paper, Scissors.js
    Actually works now.

--- Website/Rock, Paper, Scissors.js
+++ Website/Rock, Paper, Scissors.js
@@ -1,8 +1,11 @@
 botMove = ""
-let playerMove = ""
+let playerMove = null
 let result = ""
+let playerScore = 0
+let botScore = 0
 
-function botMove() {
+
+function botTurn() {
     const botAnswers = ["rock", "paper", "scissors"]
     let position = Math.floor(Math.random() * 3);
     botMove = botAnswers[position]
@@ -13,27 +16,35 @@ function myFunction(t1) {
 }
 
 function startGame() {
-    if (botMove == playerMove) {
-        result = "Draw"
-    } else if (botMove == "rock" && playerMove == "paper"){
-        result = "Win"
-    } else if (botMove == "paper" && playerMove == "scissors"){
-        result = "Win"
-    } else if (botMove == "scissors" && playerMove == "rock"){
-        result = "Win"
+    
+    if (playerMove == null){
+        alert("Please select a move before playing!")
     } else{
+        botTurn()
+
+        if (botMove == playerMove) {
+        result = "Drew"
+        } else if (botMove == "rock" && playerMove == "paper"){
+        result = "Win"
+        } else if (botMove == "paper" && playerMove == "scissors"){
+        result = "Win"
+        } else if (botMove == "scissors" && playerMove == "rock"){
+        result = "Win"
+        } else{
         result = "Lose"
+        }
+
+
+        if (result == "Win") {
+            playerScore = playerScore + 1
+        } else if (result == "Lose"){
+            botScore = botScore + 1
+        }
+
+        document.getElementById("playerScore").innerText = playerScore
+        document.getElementById("botScore").innerText = botScore
+
+        alert("The bot went " + botMove + " and you went " + playerMove + " so you " + result) 
     }
-    alert(result) 
+
 }
-
-// function callBoth(){
-//     botMove()
-//     startGame()
-// }
-
-
-// Scoreboard
-// Input validation
-// Make is look good
-// Swap between 3 pictures (RPS) then have result in the middle with each move on sides (maybe)
